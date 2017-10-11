d988aab1a7cc503a5231dbf0958a4fd1d3f1f03d
SURANRA <raju.gangadhar.suranagi@one.verizon.com>
2017-10-11 19:58:17 +0530

    version2

---
+++ fileread.js
@@ -0,0 +1,20 @@
+"use strict";
+var fs = require('fs');
+var zlib = require('zlib');
+
+var readable = fs.createReadStream(`${__dirname}/greet.txt`, { encoding : 'utf8', highWaterMark: 1 * 1024});
+/*
+
+*/
+var writable = fs.createWriteStream(__dirname + '/greetcopy.txt', {encoding: 'utf8'});
+
+var compressable = fs.createWriteStream(__dirname +'/greet.txt.gz');
+
+/*
+readable.on('data', function(chunk){
+    writable.write(chunk);
+});
+*/
+var gzip = zlib.createGzip();
+readable.pipe(writable);
+readable.pipe(gzip).pipe(compressable);
